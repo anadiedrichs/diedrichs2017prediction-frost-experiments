1dd0b01fd503c171a586f80b06d26716b8e622ca
Ana Diedrichs <ana.diedrichs@frm.utn.edu.ar>
2018-07-25 01:14:37 -0300

    evaluate for regression, MAE added, related #3

--- scripts/bnlearn-utils.R
+++ scripts/bnlearn-utils.R
@@ -7,6 +7,9 @@ library(forecast) # para metodo accuracy
 #' o = observed or real values
 RMSE = function(m, o){  sqrt(mean((m - o)^2)) } #tested
 rsq <- function(x, y){ summary(lm(y~x))$r.squared } #tested
+MAE <- function(m, o){  mean(abs(m - o))} #tested
+
+
 #' pred & obs must be numeric vectors or arrays with the same length
 #' 
 evaluate <- function(pred, obs) #tested
@@ -14,15 +17,16 @@ evaluate <- function(pred, obs) #tested
   #"RMSE","r2","Sensitivity","Acc","Precision"
   rmse <- round(RMSE(pred,obs),2)
   r2 <- round(rsq(obs,pred),2)
+  mae <- round(MAE(obs,pred),2)
   breaks <- c(-20,0,50) # caso Helada y no helada
   y <- cut(obs, breaks = breaks)
   y_pred <- cut(pred, breaks = breaks)
   sens <- round(sensitivity(y_pred,y),2)
   spec <- round(specificity(y_pred, y),2)
   p <- round(precision(y_pred,y),2)
-  c <- confusionMatrix(y_pred,y)
+  c <- confusionMatrix(y_pred,y,mode="everything")
   acc <- round(c$overall["Accuracy"],2)
-  return(list(rmse = rmse, r2 = r2, sens= sens, spec= spec, prec= p, acc= acc))
+  return(list(rmse = rmse, r2 = r2, sens= sens, spec= spec, prec= p, acc= acc,cm = c, MAE= mae))
 }
 #' pred and obs are factor vectors of the same levels. 
 evaluate.classification <- function(pred, obs) #tested
