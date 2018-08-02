6a2c72642cddc348e8cee1348ba7274bfa62eda8
Ana Diedrichs <ana.diedrichs@frm.utn.edu.ar>
2018-08-02 02:35:57 -0300

    related #8. listo para correr experimento para una estacion y un dataset

--- scripts/draft-caret-regression.R
+++ scripts/draft-caret-regression.R
@@ -29,13 +29,13 @@ SAVE_MODEL <- TRUE
 config.train <-c("normal")#,"smote")
 config.vars <-c("local","all") #only local variables or all variables.
 #' T cuantos dias anteriores tomamos
-period <- c(1,2,3,4)#,5) #TODO IN PRODUCTION
+period <- c(1)#,2,3,4)#,5) #TODO IN PRODUCTION
 #tunegrid <- expand.grid(.mtry=c(10:25),.ntree=seq(from=500,to=2500,by=500))
 # porcentaje para train set split
 porc_train = 0.68
 breaks <- c(-20,0,50) # caso Helada y no helada
 # rf: random forest, glm: logistic regression
-models <- c("rf","rpart","bnReg") #TODO IN PRODUCTION
+models <- c("bnReg","rf") #TODO IN PRODUCTION  "rpart",
 # variable cuyo valor cambia segun configuracion for
 samp = "none" 
 tuneParLen = 1 
@@ -97,9 +97,9 @@ train.models <- function(trCtrl, X, Y,data, modelName,tuneLen)
   {
     set.seed(SEED)
     # por defecto entrena en modo grilla o grid
-    Y <- cbind(Y)
-    colnames(Y) <- "Y"
-    model <- train(x= X, y = Y, data = data, 
+    Y <- as.vector(as.numeric(Y))
+    #colnames(Y) <- "Y"
+    model <- train(x= data, y = Y, data = data, 
                       method = bnReg, metric=my.metric,
                       trControl = trCtrl, 
                       node = "Y") 
@@ -139,27 +139,27 @@ for(j in 1:length(dataset)) # POR cada uno de los datasets
   
   
   
-  #foreach(p = 1:length(pred_sensores),.packages = packages) %dopar% # 
-   for(p in 2:length(pred_sensores)) 
+ # foreach(p = 1:length(pred_sensores),.packages = packages) %dopar% # 
+   for(p in 1:length(pred_sensores[1])) #solo corro Junin 
   {
     
     Log(pred_sensores[p])
    
-    # foreach(c = 1:length(config.train),.packages = packages) %dopar%  # 
-  for(ct in 1:length(config.train))
+   foreach(c = 1:length(config.train),.packages = packages) %dopar%  # 
+  #for(ct in 1:length(config.train))
     {
       Log("config.train ",config.train[ct])
       
       if(config.train[ct] == "smote"){samp = smotest}
       else {samp = NULL}
       
-      #foreach(cvars = 1:length(config.vars),.packages = packages) %dopar%  # 
-      for(cvars in 1:length(config.vars))
+      foreach(cvars = 1:length(config.vars),.packages = packages) %dopar%  # 
+      #for(cvars in 1:length(config.vars))
       {
         Log("config.vars ",config.vars[cvars])
         
-        #foreach(t = 1:length(period),.packages = packages) %dopar% 
-        for(t in period)
+        foreach(t = 1:length(period),.packages = packages) %dopar% 
+        #for(t in period)
         {
           Log("T value ",t)
           
@@ -173,11 +173,11 @@ for(j in 1:length(dataset)) # POR cada uno de los datasets
           #fila_header <- paste(dd$name,pred_sensores[p],config.train[ct],config.vars[cvars],t,sep = ",")
           fila_header <- cbind(dd$name,pred_sensores[p],config.train[ct],config.vars[cvars],t)
           name_header <- paste(dd$name,pred_sensores[p],config.train[ct],config.vars[cvars],t,sep = "--")
-          
-          for(mod in models)
+         foreach(m = 1:length(models),.packages = packages) %dopar% 
+         # for(mod in models)
           {
             Log("Model ",mod)
-            cc <- createTimeSlices(1:nrow(training.set),initialWindow=300,horizon=100,fixedWindow=FALSE)
+            cc <- createTimeSlices(1:nrow(X),initialWindow=300,horizon=100,fixedWindow=FALSE)
             KFOLDS <- length(cc$train)
             if(ncol(X)<10) tunePar = 3
             else tunePar = 10
@@ -230,7 +230,7 @@ for(j in 1:length(dataset)) # POR cada uno de los datasets
             colnames(dat)<- c("y_real","y_pred")
             write.csv(x = dat,file = paste(PATH.RESULTS,file.name,"--Y-vs-Y_pred.csv",sep = ""))
             # evaluar en testeo
-            eee <- evaluate(pred, real)
+            eee <- evaluate(pred, aux$real)
             # guardo detalles del experimento
             row <- cbind(fila,eee$MAE,eee$rmse,eee$r2,t(eee$cm$overall),t(eee$cm$byClass))
             write.table(row, file=RESUMEN, append = TRUE,row.names = FALSE,col.names = FALSE, sep = ",")
@@ -238,7 +238,7 @@ for(j in 1:length(dataset)) # POR cada uno de los datasets
 
             # agregar modelo a una lista
             #lista[[file.name]] <- model
-             warnings()
+            warnings()
           }# for each model
 
         }# for por T
