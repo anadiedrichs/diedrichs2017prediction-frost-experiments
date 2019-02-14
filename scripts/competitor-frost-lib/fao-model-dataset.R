48b6eb123d5e49d40898122ce06787b8cea9f321
Ana Diedrichs <ana.diedrichs@frm.utn.edu.ar>
2019-02-14 02:40:45 -0300

    Big changes in the script. 1) use DACC dataset (the same used in frost ieee 2018 paper) 2) use version 0.0.2 of frost library 2.1 calculate dew point using data from DACC dataset instead of darksky lib. 3) add BN model for Junin 4) add RF model for Junin We can see that BN and RF predictions are better than the empiric methods (FAO and Mdz), at least for Junin station TODO: Compare with the other four stations

--- scripts/competitor-frost-lib/fao-model-dataset.R
+++ scripts/competitor-frost-lib/fao-model-dataset.R
@@ -5,19 +5,9 @@
 
 # notebook published --> http://rpubs.com/adiedrichs/410193
 
-source("../dataset-processing.R")
-
-dataset <- get.dataset("dacc")
-
-data <- dataset$data
-
-library(caret)
-source("../metrics.R")
-# porcentaje para train set split
-porc_train = 0.68
+# variables: colnames o conjunto de variables del dataset
 
 # var: nombre variable a predecir,ejemplo *_tmin
-# variables: colnames o conjunto de variables del dataset
 vars.del.sensor <- function(var,variables,dataset_tmin_chaar=FALSE)
 {
   v <- variables
@@ -28,63 +18,72 @@ vars.del.sensor <- function(var,variables,dataset_tmin_chaar=FALSE)
   #vars <- vars[-length(vars)] # quito la última variable min_t
   return(vars)
 }
+library(caret)
+source("../dataset-processing.R")
+source("../metrics.R")
 
+dataset <- get.dataset("dacc")
+
+data <- dataset$data
+
+#dividir dataset en entrenamiento y testeo.
+### Training set y test dataset
+# porcentaje para train set split
+porc_train = 0.68
+until <- round(nrow(data)*porc_train)
+#' consideramos estacion junin
+print(dataset$pred[1])
+vars <- vars.del.sensor(dataset$pred[1],colnames(data))
+training.set = data[1:until-1, vars] # This is training set to learn the parameters
+test.set = data[until:nrow(data), vars]
+
+library(frost)
+#' Maldonado
+#' 
+
+dewpoint <- calcDewPoint(training.set$junin.humedad_med,training.set$junin.temp_med,mode = "B")
+dw.test <- calcDewPoint(test.set$junin.humedad_med,test.set$junin.temp_med,mode = "B")
+
+model.mza <- buildMdz(dw=dewpoint, tempMax=training.set$junin.temp_max, tmin=training.set$junin.temp_min)
+
+# espero un arreglo de valores. si da error, deberé usar sapply.
+predmza <- predMdz(dw = dw.test, tempMax = test.set$junin.temp_max, model=model.mza)
+
+evaluate(predmza,test.set$junin.temp_min)
+
+plot(predmza,test.set$junin.temp_min)
+
+#' FAO modelo
+#' 
+model.FAO <- buildFAO(dw=dewpoint,temp = training.set$junin.temp_med,tmin=training.set$junin.temp_min)
+
+# espero un arreglo de valores. si da error, deberé usar sapply.
+pred <- predFAO(model=model.FAO,t=test.set$junin.temp_med,dw=dw.test)
+# comparar resultados
+evaluate(pred,test.set$junin.temp_min)
+
+plot(pred,test.set$junin.temp_min)
+
+
+#' random forest
+#' 
+library(readr)
+dacc_junin_rf <- read_csv("dacc--junin.temp_min--normal--all--1--rf--Y-vs-Y_pred.csv")
+evaluate(dacc_junin_rf$y_pred,dacc_junin_rf$y_real)
+plot(dacc_junin_rf$y_pred,dacc_junin_rf$y_real)
+
+dacc_junin_bn <- read_csv("dacc--junin.temp_min--normal--all--1--bnReg--Y-vs-Y_pred.csv")
+evaluate(dacc_junin_bn$y_pred,dacc_junin_bn$y_real)
+plot(dacc_junin_bn$y_pred,dacc_junin_bn$y_real)
+
+#' ignorar desde aqui abajo
+### AHORA dejando solo los dias claros/despejados 
 #' load dataset 
 #library(readr)
 #df <- read_csv("~/phd-repos/datasets/darksky/competitor-frost-lib-iot-journal-fao-maldonado/dataset-FAO-test.csv", 
 #                             col_types = cols(time = col_character(), 
 #                                              time_dw_temp = col_character()))
 
-#' consideramos estacion junin
-est <- "junin"
-#dividir dataset en entrenamiento y testeo.
-### Training set y test dataset
-until <- round(nrow(df)*porc_train)
-training.set = df[1:until-1, ] # This is training set to learn the parameters
-test.set = df[until:nrow(df), ]
+#train <- training.set[which(training.set$icon %in% c("clear-day","partly-cloudy-day")),]
+#test <-  test.set[which(test.set$icon  %in% c("clear-day","partly-cloudy-day")),]
 
-
-library(frost)
-
-#' FAO modelo
-#' 
-model.FAO <- buildFAO(dw=training.set$dewPoint,temp = training.set$temperature,tmin=training.set$junin.temp_min)
-
-# espero un arreglo de valores. si da error, deberé usar sapply.
-pred <- predFAO(a=model.FAO$a,b=model.FAO$b,c=model.FAO$c,t=test.set$temperature,dw=test.set$dewPoint)
-# comparar resultados
-evaluate(pred,test.set$junin.temp_min)
-
-plot(pred,test.set$junin.temp_min)
-#' Maldonado
-#' 
-model.mza <- buildMdz(dw=training.set$dewPoint, tempMax=training.set$junin.temp_max, tmin=training.set$junin.temp_min)
-# espero un arreglo de valores. si da error, deberé usar sapply.
-predmza <- predMdz(dw = test.set$dewPoint, tempMax = test.set$junin.temp_max, K=model.mza$k)
-
-evaluate(predmza,test.set$junin.temp_min)
-
-plot(predmza,test.set$junin.temp_min)
-### AHORA dejando solo los dias claros/despejados 
-
-train <- training.set[which(training.set$icon %in% c("clear-day","partly-cloudy-day")),]
-test <-  test.set[which(test.set$icon  %in% c("clear-day","partly-cloudy-day")),]
- 
-model.FAO <- buildFAO(dw=train$dewPoint,temp = train$temperature,tmin=train$junin.temp_min)
-
-# espero un arreglo de valores. si da error, deberé usar sapply.
-pred <- predFAO(a=model.FAO$a,b=model.FAO$b,c=model.FAO$c,t=test$temperature,dw=test$dewPoint)
-# comparar resultados
-
-evaluate(pred,test$junin.temp_min)
-
-plot(pred,test$junin.temp_min)
-#' Maldonado
-#' 
-model.mza <- buildMdz(dw=train$dewPoint, tempMax=train$junin.temp_max, tmin=train$junin.temp_min)
-# espero un arreglo de valores. si da error, deberé usar sapply.
-predmza <- predMdz(dw = test$dewPoint, tempMax = test$junin.temp_max, K=model.mza$k)
-
-evaluate(predmza,test$junin.temp_min)
-
-plot(predmza,test$junin.temp_min)
